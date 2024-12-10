84a3a09e03dc8b2d6cbf25574262abb17bb1e86f
Caleb <jenkorbjanke@gmail.com>
2024-12-10 13:29:56 -0500

    hdugafgsy

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+//app.component.ts
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MyService } from './services/my-service.service';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent {
+    title = 'injectible-service-demo';
+
+    items: any[] = [];
+
+    constructor(private myService: MyService) {
+        this.items = this.myService.getData();
+    }
+
+    addItem() {
+        const newItem = { id: Date.now(), value: 'New Item' };
+        this.myService.addData(newItem);
+        this.items = this.myService.getData();
+    }
+}
