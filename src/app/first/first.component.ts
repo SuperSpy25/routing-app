bee5b53f9d1b7caf179585edfaf0e07d9c5ae9d1
Caleb <jenkorbjanke@gmail.com>
2024-12-17 13:58:47 -0500

    ifkhruaegygk

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-first',
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './first.component.css'
 })
 export class FirstComponent {
+
+async buttonPress() {
+constructor(private http: HttpClient) {}
+  const response = this.http.get<any>('https://pokeapi.co/api/v2/pokimon/ditto')
+  const result = await firstValueFrom(response)
+  console.log ({data: result});
+  }
 
 }
